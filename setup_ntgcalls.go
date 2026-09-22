8ca1caf995399a8ae6e2698043924b9eb47da0da
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-09-22 17:02:38 +0530

    Update ntgcalls to rc05

--- setup_ntgcalls.go
+++ setup_ntgcalls.go
@@ -26,7 +26,7 @@ import (
 const (
 	destHeader = "ntgcalls"
 	destLib    = ""
-	releaseURL = "https://api.github.com/repos/pytgcalls/ntgcalls/releases/tags/v3.0.0-rc04"
+	releaseURL = "https://api.github.com/repos/pytgcalls/ntgcalls/releases/tags/v3.0.0-rc05"
 )
 
 type Release struct {
